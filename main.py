255e976d5936c9943d7fee7e4ba86220a17c48e0
RieCo432 <colin.ries@gmail.com>
2017-12-26 01:23:27 +0100

    Finished up error handling

--- main.py
+++ main.py
@@ -2,16 +2,14 @@
 
 import requests
 import json
+import sys
 import time
-import api_keys
-
-api_key = "e798d89ba8e4debc2f3904f23e0fbaa99dc3c3a85b63924d63375b2057231a3b"
-
-request_string = "https://miningpoolhub.com/index.php?page=api&action=getuserallbalances&api_key=" + api_key
+from api_keys import api_keys
 
 bittrex_getticker_request_string = "https://bittrex.com/api/v1.1/public/getticker?market=BTC-"
 
 symbol_dict = {"auroracoin-qubit": "AUR",
+               "bitcoin": "BTC",
                "bitcoin-cash": "BCC",
                "bitcoin-gold": "BTG",
                "dash": "DASH",
@@ -24,10 +22,11 @@ symbol_dict = {"auroracoin-qubit": "AUR",
                "geocoin": "GEO",
                "groestlcoin": "GRS",
                "litecoin": "LTC",
+               "maxcoin": "MAX",
                "monacoin": "MONA",
                "monero": "XMR",
                "musicoin": "MUSIC",
-               "myriadcoin": "XMY",
+               "myriadcoin-skein": "XMY",
                "startcoin": "START",
                "verge-scrypt": "XVG",
                "vertcoin": "VTC",
@@ -38,44 +37,52 @@ symbol_dict = {"auroracoin-qubit": "AUR",
 
 
 def calculate_btc_equivalent(coin_id, amount):
-    coinmarkedcap_json = json.loads(requests.get("https://api.coinmarketcap.com/v1/ticker/").text)
+	try:
+		coinmarkedcap_json = json.loads(requests.get("https://api.coinmarketcap.com/v1/ticker/").text)
 
-    poloniex_json = json.loads(requests.get("https://poloniex.com/public?command=returnTicker").text)
+		poloniex_json = json.loads(requests.get("https://poloniex.com/public?command=returnTicker").text)
 
-    current_coin_data_coinmarketcap = filter(lambda coin: coin["id"] == coin_id, coinmarkedcap_json)
-    if coin_id in symbol_dict:
-        current_coin_data_bittrex = json.loads(
-            requests.get(bittrex_getticker_request_string + symbol_dict[coin_id]).text)
-    if coin_id in symbol_dict and current_coin_data_bittrex["success"]:
-        exchange_rate = current_coin_data_bittrex["result"]["Last"]
-        coin_btc_equivalent = amount * float(exchange_rate)
-        #print("Coin: " + str(coin_id))
-        #print("Amount: " + str(amount))
-        #print("Exchange rate: " + str(exchange_rate))
-        #print("BTC equiv. : " + str(coin_btc_equivalent) + "\n")
-        return coin_btc_equivalent
-    elif coin_id in symbol_dict and "BTC_" + symbol_dict[coin_id] in poloniex_json:
-        current_coin_data_poloniex = poloniex_json["BTC_" + symbol_dict[coin_id]]
-        exchange_rate = current_coin_data_poloniex["last"]
-        coin_btc_equivalent = amount * float(exchange_rate)
-        #print("Coin: " + str(coin_id))
-        #print("Amount: " + str(amount))
-        #print("Exchange rate: " + str(exchange_rate))
-        #print("BTC equiv. : " + str(coin_btc_equivalent) + "\n")
-        return coin_btc_equivalent
-    elif len(current_coin_data_coinmarketcap) is not 0:
-        exchange_rate = current_coin_data_coinmarketcap[0]["price_btc"]
-        coin_btc_equivalent = amount * float(exchange_rate)
-        #print("Coin: " + str(coin_id))
-        #print("Amount: " + str(amount))
-        #print("Exchange rate: " + str(exchange_rate))
-        #print("BTC equiv. : " + str(coin_btc_equivalent) + "\n")
-        return coin_btc_equivalent
-    else:
-        #print("Coin: " + str(coin_id))
-        #print("Amount: " + str(amount))
-        #print("Error: No information on exchange rate!\n")
-        return 0.0
+		current_coin_data_coinmarketcap = filter(lambda coin: coin["id"] == coin_id, coinmarkedcap_json)
+		current_coin_data_bittrex = json.loads(requests.get(bittrex_getticker_request_string + symbol_dict[coin_id]).text)
+
+		if current_coin_data_bittrex["success"]:
+			exchange_rate = current_coin_data_bittrex["result"]["Last"]
+			coin_btc_equivalent = amount * float(exchange_rate)
+			#print("Coin: " + str(coin_id))
+			#print("Amount: " + str(amount))
+			#print("Exchange rate: " + str(exchange_rate))
+			#print("BTC equiv. : " + str(coin_btc_equivalent) + "\n")
+
+			return coin_btc_equivalent
+
+		elif "BTC_" + symbol_dict[coin_id] in poloniex_json:
+			current_coin_data_poloniex = poloniex_json["BTC_" + symbol_dict[coin_id]]
+			exchange_rate = current_coin_data_poloniex["last"]
+			coin_btc_equivalent = amount * float(exchange_rate)
+			#print("Coin: " + str(coin_id))
+			#print("Amount: " + str(amount))
+			#print("Exchange rate: " + str(exchange_rate))
+			#print("BTC equiv. : " + str(coin_btc_equivalent) + "\n")
+
+			return coin_btc_equivalent
+
+		#elif len(current_coin_data_coinmarketcap) is not 0:
+			#exchange_rate = current_coin_data_coinmarketcap[0]["price_btc"]
+			#coin_btc_equivalent = amount * float(exchange_rate)
+			
+			#print("Coin: " + str(coin_id))
+			#print("Amount: " + str(amount))
+			#print("Exchange rate: " + str(exchange_rate))
+			#print("BTC equiv. : " + str(coin_btc_equivalent) + "\n")
+
+			#return coin_btc_equivalent
+		else:
+			return 0.0
+
+	except Exception as e:
+		with open("error.log", "a") as error_file:
+			error_file.write("Key Error: " + str(e) + "\n")
+		return 0.0
 
 
 def check_balance_btc_equivalent():
@@ -95,12 +102,15 @@ def check_balance_btc_equivalent():
     return total_btc_value
 
 
-def log_btc_equivalent(amount):
-    with open("/var/www/colinries.com/mining/colin.log", "a") as logfile:
+def log_btc_equivalent(amount, filename):
+    with open(filename, "a") as logfile:
         logfile.write(time.strftime("%Y/%m/%d-%H:%M:%S\t") + amount + "\n")
 
 
 while True:
-    total_btc_equivalent = check_balance_btc_equivalent()
-    log_btc_equivalent(total_btc_equivalent)
-    time.sleep(10*60)
+	for name in api_keys:
+		api_key = api_keys[name]
+		request_string = "https://miningpoolhub.com/index.php?page=api&action=getuserallbalances&api_key=" + api_key
+    		total_btc_equivalent = check_balance_btc_equivalent()
+    		log_btc_equivalent(total_btc_equivalent, name+".log")
+	time.sleep(10*60)
